ab8b66fe8244769238d931da2b7d22b4c1b7f6a7
Nicolas Rodeyro <nicolasrodeyro@gmail.com>
2025-11-27 12:19:21 -0300

    modificacion de loggers

--- api/src/logger.js
+++ api/src/logger.js
@@ -1,5 +1,22 @@
-import pino from "pino";
+import winston from 'winston';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
+const esTransportOpts = {
+  level: 'info',
+  clientOpts: { node: 'http://elasticsearch:9200' },
+  indexPrefix: 'logs-resto'
+};
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  // ETIQUETA PARA LA API:
+  defaultMeta: { service: 'api-restaurante' }, 
+  transports: [
+    new winston.transports.Console(),
+    new ElasticsearchTransport(esTransportOpts)
+  ],
 });
--- servicio-cocina/src/logger.js
+++ servicio-cocina/src/logger.js
@@ -1,8 +1,22 @@
-import pino from "pino";
+import winston from 'winston';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
-  transport: process.env.NODE_ENV === "production"
-    ? undefined
-    : { target: "pino-pretty", options: { colorize: true } }
+const esTransportOpts = {
+  level: 'info',
+  clientOpts: { node: 'http://elasticsearch:9200' },
+  indexPrefix: 'logs-resto'
+};
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  // ETIQUETA DIFERENTE PARA LA COCINA:
+  defaultMeta: { service: 'servicio-cocina' }, 
+  transports: [
+    new winston.transports.Console(),
+    new ElasticsearchTransport(esTransportOpts)
+  ],
 });
